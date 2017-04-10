8f0e5a0229710266deae9de5b43fa0ba26a4cad2
oburu <oburusolo@gmail.com>
2017-04-10 12:03:34 +0100

    mas mierda

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
+const path =require('path');
 const PORT = process.env.PORT || 8080;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.use(express.static('public'));
-
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log('Express server is up on port: ' + PORT);
 });
